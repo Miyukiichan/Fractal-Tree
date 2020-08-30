1fc5812033e4f03d982a7f62325ebbd467f76453
Miyukiichan <sam.matthews19638@gmail.com>
2020-08-30 15:55:38 +0100

    Add rotation about the origin

--- src/site.js
+++ src/site.js
@@ -27,6 +27,11 @@ function changeFg() {
   update();
 }
 
+function changeRotation() {
+  rotation = toRadians(rotationInput.value) * -1 //Make rotation go clockwise
+  update();
+}
+
 function toDegrees(rad) {
   return rad * (180/Math.PI);
 }
@@ -57,16 +62,25 @@ function draw(x, y, addAngle, length, count) {
 }
 
 function update() {
+  // Reset canvas
   ctx.clearRect(0, 0, width, height);
   ctx.fillStyle=bgColour;
   ctx.fillRect(0, 0, width, height);
+
+  // Draw first line
   ctx.strokeStyle = fgColour;
   ctx.lineWidth = lineWidth;
   ctx.beginPath();
-  ctx.moveTo(width/2,start);
-  ctx.lineTo(width/2, start - linelengthStart);
+  var x = width/2;
+  var y = start;
+  ctx.moveTo(x, y);
+  x = x - (Math.sin(rotation) * linelengthStart);
+  y = y - (Math.cos(rotation) * linelengthStart);
+  ctx.lineTo(x, y);
+  //ctx.lineTo(width/2, start - linelengthStart);
   ctx.stroke();
-  draw(width/2, start - linelengthStart, 0, linelengthStart, 1);
+  // Recursively draw the rest of the lines
+  draw(x, y, rotation, linelengthStart, 1);
 }
 
 // Value boundaries
@@ -74,6 +88,8 @@ const maxDepth = 13;
 const maxLineLengthStart = 300;
 const minLineWidth = 1;
 const maxLineWidth = 10;
+const minRotation = 0;
+const maxRotation = 360;
 
 // Default values
 const defaultLineLengthStart = maxLineLengthStart;
@@ -83,6 +99,7 @@ const defaultAmount = 13;
 const defaultBgColour = "#5F9EA0";
 const defaultFgColour = "#c16081";
 const defaultLineWidth = 2;
+const defaultRotation = minRotation;
 
 // Canvas dimensions
 const width = 3000;
@@ -99,6 +116,7 @@ var start = defaultStart;
 var bgColour = defaultBgColour;
 var fgColour = defaultFgColour;
 var lineWidth = defaultLineWidth;
+var rotation = defaultRotation;
 
 // Get input elements and set default values
 const angleInput = document.getElementById("angle");
@@ -120,6 +138,10 @@ bgColourInput.value = defaultBgColour;
 const fgColourInput = document.getElementById("fg-colour")
 fgColourInput.addEventListener("change", changeFg);
 fgColourInput.value = defaultFgColour;
+
+const rotationInput = document.getElementById("rotation");
+rotationInput.addEventListener("input", changeRotation);
+rotationInput.value = toDegrees(defaultRotation);
 
 const sliders = Array(angleInput, lengthInput);
 
